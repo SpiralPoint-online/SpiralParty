ccb388772bfdbf2cc2b7152c959bb1b723c883ed
Spiral Point <cjroussin@spiralpoint.online>
2024-11-15 11:02:48 -0600

    Update PartyCommand.java

--- src/main/java/online/spiralpoint/spigot/spiralparty/command/PartyCommand.java
+++ src/main/java/online/spiralpoint/spigot/spiralparty/command/PartyCommand.java
@@ -7,9 +7,49 @@ import org.bukkit.command.TabExecutor;
 import java.util.List;
 
 public final class PartyCommand implements TabExecutor {
+
+    private static PartyCommand INSTANCE;
+
+    public static PartyCommand getInstance() {
+        if(PartyCommand.INSTANCE == null) PartyCommand.INSTANCE = new PartyCommand();
+        return PartyCommand.INSTANCE;
+    }
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        //TODO: Check label to see which subcommand was used
+        if(sender instanceof Player player) {
+            switch(command.getName().toLowerCase()) {
+                case "party":
+                    if(args.length == 0) return false;
+                    switch(args[0].toLowerCase()) {
+                        case "invite":
+                            String[] newArgs = String[args.length - 1];
+                            System.arraycopy(args, 2, newArgs, 1, newArgs.length);
+                            this.invite(player, newArgs);
+                            break;
+                        case "join":
+                            break;
+                        case "leave":
+                            break;
+                        default:
+                            return false;
+                    }
+                    break;
+                case "partyinvite":
+                    this.invite(player, args);
+                    break;
+                case "partyjoin":
+                    this.join(player, args);
+                    break;
+                case "partyleave":
+                    this.leave(player, args);
+                    break;
+                default:
+                    return false;
+            }
+        } else {
+            sender.sendMessage("Only players may use this command");
+        }
         return true;
     }
 
@@ -17,4 +57,14 @@ public final class PartyCommand implements TabExecutor {
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         return List.of();
     }
+
+    private void invite(Player player, String[] args) {
+    }
+
+    private void join(Player player, String[] args) {
+    }
+
+    private void leave(Player player, String[] args) {
+    }
+
 }
