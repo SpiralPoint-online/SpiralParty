11af00993a80bdbdffdbafae81bd1ec70ed5503a
Spiral Point <cjroussin@spiralpoint.online>
2024-11-15 15:54:31 -0600

    Update SpiralPartyPlugin.java

--- src/main/java/online/spiralpoint/spigot/spiralparty/SpiralPartyPlugin.java
+++ src/main/java/online/spiralpoint/spigot/spiralparty/SpiralPartyPlugin.java
@@ -2,13 +2,13 @@ package online.spiralpoint.spigot.spiralparty;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-//TODO: Change all player.sendMessage method calls to use ChatColor.translateAlternateColorCode instead of ChatColor enums directly.
 public final class SpiralPartyPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-					this.saveDefaultConfig();
+	this.saveDefaultConfig();
         SpiralPartyConfig.get(); // Initialize Config Class Singleton, do nothing with return value.
+	PluginCommand partyCommand = this.getCommand("party");
     }
 
     @Override
